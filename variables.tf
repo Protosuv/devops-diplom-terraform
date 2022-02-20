f0434bb186700e05d827dec8f8f039fc1646cac6
Alexey Suvorov <protosuv@gmail.com>
2022-02-20 23:59:20 +0300

    Add tf files

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "aws-region" {
+  default = "us-east-2"
+  description = "Default Amazon region"
+}
+variable "aws-av-zone" {
+  default = "us-east-2a"
+  description = "Avaliability zone"
+}
+
+locals {
+web_instance_type_map = {
+  default = "t2.micro"
+  stage = "t2.micro"
+  }
+web_instance_count_map = {
+  default = 1
+  stage = 3
+  }
+instances = {
+  "t2.micro" = data.aws_ami.ubuntu.id
+  }
+}
