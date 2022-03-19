41990d04d125a90c7b40d90d65723ccbf0e1dd69
Alexey Suvorov <protosuv@gmail.com>
2022-03-19 22:09:18 +0300

    Edit network.tf

--- network.tf
+++ network.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "diplom_vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-2a"]
+  azs             = ["us-east-1a"]
   public_subnets  = ["10.0.101.0/24"]
 
   enable_nat_gateway = true
